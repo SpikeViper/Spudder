3e130bb218d1c1767ebd77d8c232a83554e9088a
BuildTools <kobe.albright@gmail.com>
2020-08-26 13:22:13 -0400

    Added database context

---
+++ Spudder/SpudderDB.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spudder
+{
+    public class SpudderDB : DbContext
+    {
+
+        public static string ConnectionString = $"server={DBConfig.instance.Host};port=3306;database=spudder;uid={DBConfig.instance.Username};pwd={DBConfig.instance.Password};SslMode=Required;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(ConnectionString, options => options.EnableRetryOnFailure().CharSet(CharSet.Utf8Mb4).ServerVersion(new Version(8, 0, 20), ServerType.MySql));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Spudder/Startup.cs
+++ Spudder/Startup.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
 
 namespace Spudder
 {
@@ -23,6 +28,38 @@ namespace Spudder
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Set up database
+
+            DBConfig dbConfig = null;
+
+            string dbConfigPath = Directory.GetCurrentDirectory() + "/DBConfig.json";
+
+            if (File.Exists(dbConfigPath))
+            {
+                string data = File.ReadAllText(dbConfigPath);
+                dbConfig = JsonConvert.DeserializeObject<DBConfig>(data);
+            }
+            else
+            {
+                dbConfig = new DBConfig()
+                {
+                    Host = "INSERT HOST",
+                    Password = "INSERT PASSWORD",
+                    Username = "INSERT USERNAME"
+                };
+
+                string data = JsonConvert.SerializeObject(dbConfig);
+
+                File.WriteAllText(dbConfigPath, data);
+            }
+
+            DBConfig.instance = dbConfig;
+
+            services.AddDbContextPool<SpudderDB>(options =>
+            {
+                options.UseMySql(SpudderDB.ConnectionString, options => options.EnableRetryOnFailure().CharSet(CharSet.Utf8Mb4).ServerVersion(new Version(8, 0, 20), ServerType.MySql));
+            });
+
             services.AddControllersWithViews();
         }
 
