f3bf5e9a76585e2b4067cc4685e444a9664f29f6
superjacobl <jacoblower26@gmail.com>
2020-08-27 21:02:53 -0400

    Added Basic User Class

---
+++ Spudder/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spudder.Models
+{
+    public class Spud
+    {
+        [Key]
+        public string Svid { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        //0: False
+        //1: True
+        public int Verified { get; set; }
+        public string Avatar { get; set; }
+    }
+}
